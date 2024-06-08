37d91485ceb28a1a96420df80cbb3eb611f8a74f
stefan judis <stefanjudis@gmail.com>
2024-06-08 15:19:16 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+// Import the framework and instantiate it
+import Fastify from "fastify";
+import { chromium } from "playwright";
+const fastify = Fastify({
+  logger: true,
+});
+
+function checkUrl(string) {
+  var url = "";
+  try {
+    url = new URL(string);
+  } catch (error) {
+    return false;
+  }
+  return true;
+}
+
+fastify.get("/", async function handler(req, reply) {
+  const { query } = req;
+
+  if (!query.url) return { statusCode: 400, body: "No url query specified." };
+
+  if (!checkUrl(query.url))
+    return { statusCode: 400, body: "No url query specified." };
+
+  try {
+    const browser = await chromium.launch();
+    const page = await browser.newPage({
+      screen: {
+        width: 1000,
+        height: 600,
+      },
+    });
+    await page.goto(decodeURIComponent(query.url));
+    const screenshot = await page.screenshot({ path: "screenshot.jpg" });
+
+    reply.code(200);
+    reply.header("content-type", "image/jpeg");
+    reply.send(screenshot);
+
+    // return {
+    //   statusCode: 200,
+    //   headers: {
+    //     "content-type": `image/jpeg`,
+    //   },
+    //   body: screenshot.toString("base64"),
+    //   isBase64Encoded: true,
+    // };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: "The server encountered an error. You may have inputted an invalid query.",
+    };
+  }
+});
+
+try {
+  await fastify.listen({ port: 3000 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
